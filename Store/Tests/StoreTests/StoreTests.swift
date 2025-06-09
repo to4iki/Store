9023638391ace24d38063d3a3f07926c986f5f25
Toshiki Takezawa <tsk.take815@gmail.com>
2025-06-10 00:59:29 +0900

    Add simple store tests

--- Store/Tests/StoreTests/StoreTests.swift
+++ Store/Tests/StoreTests/StoreTests.swift
@@ -2,6 +2,39 @@ import Testing
 
 @testable import Store
 
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite
+struct StoreTests {
+  struct State: Sendable {
+    var count: Int = 0
+  }
+
+  struct Action {
+    let increment: () -> Void
+    let decrement: () -> Void
+  }
+
+  @MainActor
+  func useStore() -> (store: Store<State>, action: Action) {
+    createStore(initialState: State()) { set in
+      Action(
+        increment: {
+          set { $0.count += 1 }
+        },
+        decrement: {
+          set { $0.count -= 1 }
+        }
+      )
+    }
+  }
+
+  @MainActor
+  @Test
+  func setState() {
+    let (store, action) = useStore()
+    #expect(store.state.count == 0)
+    action.increment()
+    #expect(store.state.count == 1)
+    action.decrement()
+    #expect(store.state.count == 0)
+  }
 }
